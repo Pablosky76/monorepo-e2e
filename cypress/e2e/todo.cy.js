a19a3e6c170d679d07fa6a5af0b9e341f0987b6c
Pablo <147501769+Pablosky76@users.noreply.github.com>
2025-10-02 20:27:43 +0200

    Añadidos tests de TodoMVC en todo.cy.js

---
+++ cypress/e2e/todo.cy.js
@@ -0,0 +1,73 @@
+/// <reference types="cypress" />
+
+const selectors = {
+  newTodo: '.new-todo',
+  todoItems: '.todo-list li',
+  toggle: '.toggle',
+  destroy: '.destroy',
+  label: 'label',
+  edit: '.edit',
+  filterAll: 'a[href="#/"]',
+  filterActive: 'a[href="#/active"]',
+  filterCompleted: 'a[href="#/completed"]',
+}
+
+describe('TodoMVC - Ejercicios Cypress', () => {
+  beforeEach(() => {
+    cy.visit('https://todomvc.com/examples/react/#/')
+  })
+
+  it('1) Crear tarea', () => {
+    cy.get(selectors.newTodo).type('Comprar pan{enter}')
+    cy.get(selectors.todoItems).should('have.length', 1)
+    cy.get(selectors.todoItems).first().find(selectors.label).should('have.text', 'Comprar pan')
+  })
+
+  it('2) Marcar tarea como completada', () => {
+    cy.get(selectors.newTodo).type('Tarea completada{enter}')
+    cy.get(selectors.todoItems).first().find(selectors.toggle).check()
+    cy.get(selectors.todoItems).first().should('have.class', 'completed')
+  })
+
+  it('3) Desmarcar tarea completada', () => {
+    cy.get(selectors.newTodo).type('Tarea a desmarcar{enter}')
+    cy.get(selectors.todoItems).first().find(selectors.toggle).check()
+    cy.get(selectors.todoItems).first().should('have.class', 'completed')
+    cy.get(selectors.todoItems).first().find(selectors.toggle).uncheck()
+    cy.get(selectors.todoItems).first().should('not.have.class', 'completed')
+  })
+
+  it('4) Editar tarea', () => {
+    cy.get(selectors.newTodo).type('Nombre original{enter}')
+    cy.get(selectors.todoItems).first().find(selectors.label).dblclick()
+    cy.get(selectors.todoItems).first().find(selectors.edit).clear().type('Nombre editado{enter}')
+    cy.get(selectors.todoItems).first().find(selectors.label).should('have.text', 'Nombre editado')
+  })
+
+  it('5) Borrar tarea', () => {
+    cy.get(selectors.newTodo).type('Borrar esta tarea{enter}')
+    cy.get(selectors.todoItems).first().trigger('mouseover')
+    cy.get(selectors.todoItems).first().find(selectors.destroy).click({ force: true })
+    cy.get(selectors.todoItems).should('have.length', 0)
+  })
+
+  it('6) Filtrar tareas', () => {
+    cy.get(selectors.newTodo).type('Tarea 1{enter}')
+    cy.get(selectors.newTodo).type('Tarea 2{enter}')
+    cy.get(selectors.newTodo).type('Tarea 3{enter}')
+
+    cy.get(selectors.todoItems).eq(1).find(selectors.toggle).check()
+
+    cy.contains('Completed').click()
+    cy.get(selectors.todoItems).should('have.length', 1).and('have.class', 'completed')
+
+    cy.contains('Active').click()
+    cy.get(selectors.todoItems).should('have.length', 2)
+    cy.get(selectors.todoItems).each($li => {
+      expect($li).not.to.have.class('completed')
+    })
+
+    cy.contains('All').click()
+    cy.get(selectors.todoItems).should('have.length', 3)
+  })
+})
